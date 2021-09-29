5471304e9e86f0344e63678a406abb8346bffa11
Craig baldry <cbaldry@our.ecu.edu.au>
2021-09-29 20:39:26 +0800

    fix network key

--- sensors/heartsensor.py
+++ sensors/heartsensor.py
@@ -14,9 +14,10 @@ import datetime
 
 
 class HeartSensor:
-    NETWORK_KEY = [0xB9, 0xA5, 0x21, 0xFB, 0xBD, 0x72, 0xC3, 0x45]
+    
     
     def __init__(self, on_data):
+        self.NETWORK_KEY = [0xB9, 0xA5, 0x21, 0xFB, 0xBD, 0x72, 0xC3, 0x45]
         self.node = Node()    
         self.node.set_network_key(0x00, NETWORK_KEY)
         self.channel = node.new_channel(Channel.Type.BIDIRECTIONAL_RECEIVE)
